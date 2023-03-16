693cc162fdd4f45b5b85ce1cbcf4a96a161885c8
NoahKKA <noahhhhh.a@gmail.com>
2023-03-16 00:34:40 -0700

    finished

--- public/admin.js
+++ public/admin.js
@@ -1,2 +1,44 @@
 
+async function listBooks(){
+let response = await fetch("http://localhost:3001/listBooks",{
+    method: "GET",
+});
+let listBook = await response.json();
+
+let list = document.createElement("ul");
+
+listBook.forEach(function(book){
+  let input = document.createElement('input')
+  input.type = 'number'
+  input.value = ""
+  input.placeholder = "Quantity."
+  
+  let sumbitButton = document.createElement('button');
+  sumbitButton.innerHTML = 'Save';
+  sumbitButton.addEventListener("click", async function(){
+    let res = await fetch("http://localhost:3001/updateBook",{
+        method:"PATCH",
+        headers:{
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+            'id': book.id,
+            'quantity':input.value
+        })
+    })
+let updateBooks = await res.json()
+console.log(updateBooks)
+  })
+
+  let listItem = document.createElement("li");
+  listItem.innerHTML = `${book.title}`;
+  list.appendChild(listItem);
+  listItem.appendChild(input)
+  listItem.appendChild(sumbitButton)
+})
+
+let container = document.querySelector('#root')
+container.appendChild(list)
+}
+listBooks()
 // Your Code Here
